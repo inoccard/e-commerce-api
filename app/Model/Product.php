59c25234d9a417d6716d847b8346eff5673934dc
inoccard <inocenciocardoso19@gmail.com>
2019-03-13 13:45:31 -0300

    Created Relationship

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -5,6 +5,10 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
-{
-    //
+{   
+    // Relacionamento muitos para um
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    // relacionamento um para muitos
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
